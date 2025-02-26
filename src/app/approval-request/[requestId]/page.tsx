6caf14c334e8066842044ec6e1743469935ba042
jiHeeFlee <esder1310@naver.com>
2025-02-26 22:33:38 +0900

    :tada feat : 파일 카테고리 기능 구현

--- src/app/approval-request/[requestId]/page.tsx
+++ src/app/approval-request/[requestId]/page.tsx
@@ -2,11 +2,27 @@
 
 "use client";
 
-export default function ApprovalPage() {
+import { useState,useEffect } from "react";
+import Category from "@/shared/Category";
 
+
+export default function ApprovalPage() {
+    const [fileClick, setFileClick]=useState<string>('1분 프레젠테이션');
+    const handleFileCategory=(file:string)=>{
+        setFileClick(file);
+        console.log('📝 선택한 파일명 : ', file);
+    }
+
+    useEffect(()=>{
+        setFileClick('1분 프레젠테이션');
+    },[])
     return(
         <>
             PT 심사 페이지 - 상세 심사 페이지 :)
+            <Category 
+                onClick={handleFileCategory}
+                selectedFiled={fileClick}
+            />
         </>
     )
 }
