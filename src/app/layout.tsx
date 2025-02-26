ca8a12450823e30037fe21d3d6f224685988f587
choigyulee <cgl1409@gmail.com>
2025-02-26 23:58:03 +0900

    ✨Feat: 레이아웃 별 Gnb 표시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Gnb />
         {children}
         <Footer />
       </body>
