2e7cfdf35b1be14b8c22ff7cf2d0671f7c6a6f48
jiHeeFlee <esder1310@naver.com>
2025-02-27 20:27:59 +0900

    :memo docs : 페이지 네이션과 스타일 추가

--- src/app/approval-request/page.tsx
+++ src/app/approval-request/page.tsx
@@ -5,11 +5,16 @@
 import Dashboard from "@/shared/Dashboard";
 import Pagination from "@/shared/Pagination";
 
+import { Container } from '../../styles/approval-request/style';
+
 export default function Approval() {
 
     return (
-        <>
+        <Container>
             <Dashboard selectedTab={"프레젠테이션 승인 요청"} />
-        </>
+            <Pagination current_page={3} total_page={10}
+                onPageChange={()=>console.log('click')}
+            />
+        </Container>
     )
 }
